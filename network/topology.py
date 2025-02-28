b98ff5c7ba345503a1dca5bac90b2e2ef8aca3d2
Antonio Gelain <antonio.gelain2@gmail.com>
2025-02-28 14:21:04 +0100

    fix(topology): wrong variable name

--- network/topology.py
+++ network/topology.py
@@ -9,29 +9,29 @@ class Topology(Topo):
         super().__init__()
 
     def build(self):
-        self.switches = []
-        self.hosts = []
+        self._switches = []
+        self._hosts = []
 
         # Create and link switches
         for i, switch in enumerate(self.switches_topology):
-            self.switches.append(self.addSwitch('s' + str(i)))
+            self._switches.append(self.addSwitch('s' + str(i)))
 
         for i, switch in enumerate(self.switches_topology):
             for s in switch:
-                s1 = self.switches[i]
-                s2 = self.switches[s]
+                s1 = self._switches[i]
+                s2 = self._switches[s]
                 self.addLink(s1, s2, cls=TCLink, bw=40, delay='15ms')
 
         # Create and link hosts
         cnt = 0
         for i, host_count in enumerate(self.hosts_per_switch):
-            self.hosts.append([])
+            self._hosts.append([])
             for h in range(host_count):
                 cnt += 1
                 host = self.addHost(
                     "h" + str(cnt),
                     mac='00:00:00:00:00:0' + str(cnt)
                 )
-                self.hosts[i].append(host)
-                self.addLink(self.switches[i], host,
+                self._hosts[i].append(host)
+                self.addLink(self._switches[i], host,
                              cls=TCLink, bw=40, delay='15ms')
