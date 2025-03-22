00403b10e419c44b65fd15e6552d454934d30803
Antonio Gelain <antonio.gelain2@gmail.com>
2025-03-22 17:15:53 +0000

    refactor: small refactor

--- network/network.py
+++ network/network.py
@@ -4,6 +4,33 @@ from mininet.node import OVSKernelSwitch, RemoteController
 from mininet.link import TCLink
 from mininet.cli import CLI
 
+def setup_ftp_server(host):
+    result = host.cmd("apt install --yes vsftpd")
+    print(result)
+
+def setup(net):
+    hosts = net.hosts
+
+    # Setup every host
+    for i, host in enumerate(hosts):
+        # Skip NAT host
+        if "nat" in host.name:
+            continue
+
+        # Setup DNS for every host interface
+        interfaces = host.cmd("ls /sys/class/net") 
+        for intf in interfaces.split(' '):
+            if host.name not in intf:
+                continue
+            host.cmd(f"resolvectl dns {intf} 8.8.8.8")
+        host.cmd(f"systemctl restart systemd-resolved")
+
+        print(f"{i}. {host.name}")
+
+    # ftp_server = hosts[0]
+    # setup_ftp_server(ftp_server)
+
+
 def run():
     # Adjacency list for the switch topology
     switches = [
@@ -31,7 +58,10 @@ def run():
     # Build and start the network
     net.build()
     net.addNAT().configDefault()
+
+    setup(net)
     net.start()
+
 
     if __name__ == "__main__":
         CLI(net)
--- network/topology.py
+++ network/topology.py
@@ -27,29 +27,26 @@ class Topology(Topo):
         """
         Build a new network from the previously given topology
         """
-        self._switches = []
-        self._hosts = []
 
         # Create and link switches
+        switches = []
         for i, switch in enumerate(self.switches_topology):
-            self._switches.append(self.addSwitch('s' + str(i)))
+            switches.append(self.addSwitch('s' + str(i)))
 
         for i, switch in enumerate(self.switches_topology):
             for s in switch:
-                s1 = self._switches[i]
-                s2 = self._switches[s]
+                s1 = switches[i]
+                s2 = switches[s]
                 self.addLink(s1, s2, cls=TCLink, bw=40, delay='15ms')
 
         # Create and link hosts
         cnt = 0
         for i, host_count in enumerate(self.hosts_per_switch):
-            self._hosts.append([])
             for h in range(host_count):
                 cnt += 1
-                host = self.addHost(
-                    "h" + str(cnt),
+                hostname = self.addHost(
+                    f"h{i}_{h}",
                     mac='00:00:00:00:00:0' + str(cnt)
                 )
-                self._hosts[i].append(host)
-                self.addLink(self._switches[i], host,
+                self.addLink(switches[i], hostname,
                              cls=TCLink, bw=40, delay='15ms')
